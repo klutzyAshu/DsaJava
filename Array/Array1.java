0b11b6340bc89739e72a5feb951bf5f4396c013b
Aashu12exe <aashusen2006@gmail.com>
2026-09-01 10:02:35 +0530

    idk2

---
+++ Array/Array1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Array1{
+    public static void main(String[] args){
+        ArrayList<String> cars = new ArrayList<String>();
+        
+        cars.add("bmw");
+        cars.add("valvo");
+        cars.add("neno");
+        cars.add("diablo");
+        System.out.println(cars);
+    }
+}
