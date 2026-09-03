64c9453c274b0877e00e05ce2c94340958a2dba1
Gaurishankar sen <aashusen2006@gmail.com>
2026-09-03 16:01:23 +0530

    day2

---
+++ LA.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class LA{
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(1);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+
+    }
+
+    }
+
