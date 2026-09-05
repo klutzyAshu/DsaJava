366da1de93859d143a33bf3a7aed849f7c2f9c9c
Aashu12exe <aashusen2006@gmail.com>
2026-09-05 22:18:56 +0530

    changes 3

--- Array/Array2.java
+++ Array/Array2.java
@@ -4,6 +4,9 @@ public class Array2{
         int[] arr = {12,34,53,3,1224,43,6,4};
         for (int i , i = [-1],i++){
             system.out.println(i);
+            //idk but changed . 
+
+            
         }
         
     }
