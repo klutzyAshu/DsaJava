d9338f31af962084c7339ff740502e6e524eed80
Aashu12exe <aashusen2006@gmail.com>
2026-09-05 12:07:31 +0530

    hello

---
+++ Array/Array2.java
@@ -0,0 +1,10 @@
+public class Array2{
+
+    public static void main(String [] args){
+        int[] arr = {12,34,53,3,1224,43,6,4};
+        for (int i , i = [-1],i++){
+            system.out.println(i);
+        }
+        
+    }
+}
